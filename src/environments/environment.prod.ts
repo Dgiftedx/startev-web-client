d847821cd91e36c9345fbe2b814339111216cf66
Olubunmi Victor <olubunmivictor@winexviv.com>
2019-06-11 11:08:08 +0100

    production en

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  cloudinary: {
+  	cloud_name : 'startev', 
+    api_key: '257256259453978', 
+    api_secret: '39Q7GNO2ZJTDqrtVcJaxIvdEC_A', 
+    upload_preset: 'tvje9bug'
+  },
+  laddaConfig : {
+    style: "expand-right",
+    spinnerSize: 35,
+    spinnerLines: 15
+  }
 };
